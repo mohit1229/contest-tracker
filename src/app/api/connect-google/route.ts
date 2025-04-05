d2023a4253579cbcca8ee4efb77039e7f51883f9
mohit1229 <kumarmohiiit123@gmail.com>
2025-04-05 23:45:01 +0530

    route changed for production

--- src/app/api/connect-google/route.ts
+++ src/app/api/connect-google/route.ts
@@ -5,7 +5,7 @@ export async function GET() {
   const oauth2Client = new google.auth.OAuth2({
     clientId: process.env.GOOGLE_CLIENT_ID!,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    redirectUri: "http://localhost:3000/api/google/callback", // must match Google console
+    redirectUri: "https://syntaxcontest.vercel.app/api/google/callback", // must match Google console
   })
 
   const authUrl = oauth2Client.generateAuthUrl({
